a7d3f845d58eb5a8c84bb9caa132d5590e3c1b96
nekrxs <122643715+Nekros-dsc@users.noreply.github.com>
2024-05-15 13:29:36 +0200

    Update help.js

--- SlashCommands/Public/help.js
+++ SlashCommands/Public/help.js
@@ -9,7 +9,7 @@ const Discord = require('discord.js');
     async run(bot, message) {
         const embed = new Discord.EmbedBuilder()
         .setTitle('`🪄` ▸ Help Menu')
-        .setDescription(`\`/help\`\n*— Permet de voir la liste des commandes du bot.*\n\`/updatebot\`\n*— Permet de renouveler votre bot.*\n\`/create\`\n*— Permet de créer un bot*\n\`/mybot\`\n*— Permet de voir les bots d'un utilisateur.*\n\`/manage\`\n*— Permet de gérer vos bots.*\n\`/createkey\`\n*— Permet de créer une/des clé(s) pour un bot.*`)
+        .setDescription(`\`/help\`\n*— Permet de voir la liste des commandes du bot.*\n\`/updatebot\`\n*— Permet de renouveler votre bot.*\n\`/create\`\n*— Permet de créer un bot.*\n\`/mybot\`\n*— Permet de voir les bots d'un utilisateur.*\n\`/manage\`\n*— Permet de gérer vos bots.*\n\`/createkey\`\n*— Permet de créer une/des clé(s) pour un bot.*`)
         .setFooter({ text: message.user.username, iconURL: message.user.displayAvatarURL({ dynamic: true })})
         .setTimestamp()
         .setColor(config.color);
