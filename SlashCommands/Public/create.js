9181d812271ad01b267fee01b0cdd4bd12248a12
Shinetsu_Ruwin <110337782+Ruwin-dsc@users.noreply.github.com>
2024-05-15 20:43:14 +0200

    Update create.js

--- SlashCommands/Public/create.js
+++ SlashCommands/Public/create.js
@@ -156,7 +156,7 @@ async function copierArborescence(sourceDir, destinationParentDir, destinationFo
     const configachanger = require(`../../UsersBots/${client.user.id}/config.json`)
     if(configachanger) configachanger.token = token, configachanger.buyers.push(message.user.id)
 
-    fs.writeFileSync(`./usersbots/${client.user.id}/config.json`, JSON.stringify(configachanger, null, 2));
+    fs.writeFileSync(`./UsersBots/${client.user.id}/config.json`, JSON.stringify(configachanger, null, 2));
 
     await message.channel.send({ embeds: [embedStart5] })
     await message.channel.send({ embeds: [embedStart6] })
